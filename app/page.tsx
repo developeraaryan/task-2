b55070297de7c28975b939cc7b2c083e76b40976
Aryan Kumar <Aryanak9163@gmail.com>
2024-06-26 17:40:31 +0530

    chore: Update page.tsx with improved container styling and breakpoints

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ const HomePage: React.FC = () => {
   }, []);
 
   return (
-    <main className="container mx-auto px-2 min-h-screen py-8 flex flex-col justify-center items-center gap-10 md:mx-auto">
+    <main className="container mx-auto px-4 md:px-10 min-h-screen py-8 flex flex-col justify-center items-center gap-10 md:mx-auto">
       {users?.length > 0 ? (
         <>
           <h1 className="text-5xl md:text-9xl font-extrabold bg-gradient-to-r from-[#FF9800] via-[#FF5722] to-[#FFC107] bg-clip-text  text-transparent">
